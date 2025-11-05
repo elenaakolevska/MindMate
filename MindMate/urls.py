7042611cae9a7c9d07942ede6aed9c7528b754d6
Ljubica Jovanoska <lubicaj724@gmail.com>
2025-11-05 19:58:25 +0100

    Added home page

--- MindMate/urls.py
+++ MindMate/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from MindMateAPP import views as app_views
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -23,6 +24,7 @@ from django.urls import include
 from MindMateAPP.admin import custom_admin_site
 
 urlpatterns = [
+    path('', app_views.home, name='home'),
     path('admin/', custom_admin_site.urls),
     path('admin/', include('django.contrib.auth.urls')),  # Add default admin auth URLs
 ]
--- MindMateAPP/views.py
+++ MindMateAPP/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user
 
 # Create your views here.
+
+def home(request):
+	if request.user.is_authenticated:
+		return redirect('dashboard')  # Change 'dashboard' to your logged-in landing page name
+	return render(request, 'home.html')
