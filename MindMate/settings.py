b1c3a5e9509ab9d28ec71a785bbe88c767789876
kolevas <snezhana.koleva@students.finki.ukim.mk>
2026-02-07 17:36:37 +0100

    Bug fix

--- MindMate/settings.py
+++ MindMate/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB', 'MindMateDb'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),  # localhost for local dev, 'db' for docker-compose
+        'HOST': os.environ.get('DB_HOST', 'db'),  # 'db' is the service name in docker-compose
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
